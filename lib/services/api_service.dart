46ac9f87f250b76803aa9c7b3e860753138c0ecc
swastikgavhane789 <swastikgavhane789@gmail.com>
2025-09-21 22:11:35 +0530

    Update api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseUrl = "http://10.0.2.2:8000"; 
+  static const String baseUrl = "https://news-validation-app-173608583247.asia-south1.run.app/"; 
   // Replace with your backend IP when testing on a real device
 
   static Future<Map<String, dynamic>> verifyText(String text) async {
